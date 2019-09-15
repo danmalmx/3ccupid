3469b9ac552684dff5af1376410558c5ae83fb1b
danmalmx <danmalmx@gmail.com>
2019-09-15 18:42:23 +0200

    Reverting to previous commit

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const port = 3000;
 //Setup the view engine - expresss-handelbars
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
-
 app.get('/', (req, res) => {
   res.render('home');
 });
 
+<<<<<<< HEAD
 app.get('/about', (req, res) => {
   res.render('about');
 });
@@ -20,6 +20,8 @@ app.get('/contact', (req, res) => {
   res.render('contact');
 });
 
+=======
+>>>>>>> parent of b9be639... install nodemod
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
