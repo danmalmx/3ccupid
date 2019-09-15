1fc6b902cc51f5d98095d8e10413801ba37ebb6d
danmalmx <danmalmx@gmail.com>
2019-09-15 19:32:01 +0200

    Adding footer

--- server.js
+++ server.js
@@ -8,15 +8,21 @@ const port = 3000;
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.get('/', (req, res) => {
-  res.render('home');
+  res.render('home', {
+    title: 'Home'
+  });
 });
 
 app.get('/about', (req, res) => {
-  res.render('about');
+  res.render('about', {
+    title: 'About'
+  });
 });
 
 app.get('/contact', (req, res) => {
-  res.render('contact');
+  res.render('contact', {
+    title: 'Contact'
+  });
 });
 
 app.listen(port, () => {
