7daf0f9e705bd72e71431d1ba915ad6216c77092
danmalmx <danmalmx@gmail.com>
2019-09-15 14:08:45 +0200

    Set-up the view engine in server.js
    Express-Handlebars

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const exphbs = require('express-handlebars')
 const app = express();
 const port = 3000;
 
+//Setup the view engine - expresss-handelbars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 app.get('/', (req, res) => {
   res.send('Hello ATCCs!')
 });
